a3da9456204388b6f944fbd6a3adeb646021f3b1
James-lee978 <sgn07108@gmail.com>
2026-05-15 02:38:43 +0900

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,29 @@
+import path from "path";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  turbopack: {
+    root: path.resolve(process.cwd()),
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "firebasestorage.googleapis.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
